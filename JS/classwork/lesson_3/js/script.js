b4df70cdd33340c18bc821045114c4a4db007bba
Arlecuno <palchinskij19@gmail.com>
2020-09-07 21:46:52 +0300

    finished lesson 3

---
+++ JS/classwork/lesson_3/js/script.js
@@ -0,0 +1,20 @@
+switch (9) {
+   case '9':
+      console.log('Hi222');
+      break;
+
+   case 'some2':
+      console.log('Hi2');
+      break;
+
+   default:
+      console.log('no');
+      break;
+
+   case 'some':
+      console.log('Hi');
+      break;
+}
+
+
+
