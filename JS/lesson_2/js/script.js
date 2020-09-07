0406e795e96f5f8c962c7ebe089a84f6a575fe9d
Arlecuno <palchinskij19@gmail.com>
2020-09-07 10:38:34 +0300

    finished lesson 2

---
+++ JS/lesson_2/js/script.js
@@ -0,0 +1,42 @@
+// var some = 'halo';
+// some = 10;
+// console.log(some);
+
+// console.log(typeof (some));
+
+
+// var title = 'var';
+// console.log(title);
+
+// var imya = 'Vasyan';
+// var age = 18;
+// var isMan = true;
+
+// var person = {
+//    name: 'Vasyan',
+//    age: 18,
+//    isMan: true
+// }
+
+// console.log('This is ' +
+//    person.name + '. ' +
+//    (person.isMan ? 'His' : 'Her') + ' age is ' +
+//    person.age
+// );
+
+// console.log(!person.name);
+
+// var age = 20;
+// var someAge = 'one';
+
+// console.log(typeof (age - someAge));
+
+
+var person = {
+   name: 'Vasyan',
+   age: 18
+};
+
+var person2 = person;
+
+console.log(person2 === person);
