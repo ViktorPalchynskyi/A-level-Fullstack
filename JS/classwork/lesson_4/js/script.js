887548210efd0e9fdb78e3924159542f6b2b76c1
Arlecuno <palchinskij19@gmail.com>
2020-09-10 21:49:54 +0300

    finished lesson 4

---
+++ JS/classwork/lesson_4/js/script.js
@@ -0,0 +1,57 @@
+// var someArray = ['chiki', 'briki', 'v', 'damki'];
+
+// someArray[4] = 'maslinu';
+// someArray.push('slovil');
+// someArray.unshift('op');
+
+// console.log(someArray);
+
+// var lastEl = someArray.pop();
+// var firsEl = someArray.shift();
+
+
+// console.log(lastEl);
+// console.log(firsEl);
+
+// console.log(someArray);
+
+// console.log(someArray.lastIndexOf('v'));
+// console.log(someArray.splice(0, 2));
+
+// var sho = 'sho';
+// var newSho = sho.slice(0, 2);
+// console.log(newSho);
+
+// // crud create(insert) read update delete
+
+
+// var kotiky = [
+//    'manchkin',
+//    'sfinx',
+//    'scottish',
+//    'persian',
+//    'burma',
+//    'bengal',
+//    'metis',
+//    'chert'
+// ];
+
+// var removed = kotiky.splice(0, 2, 'cheshyr', 'sheshyr');
+// var removed2 = kotiky.slice(5, 8);
+
+
+
+// console.log(kotiky);
+// console.log(removed2);
+
+// var kotiky2 = new Array(
+//    'manchkin',
+//    'sfinx',
+//    'scottish',
+//    'persian',
+//    'burma',
+//    'bengal',
+//    'metis');
+
+
+
