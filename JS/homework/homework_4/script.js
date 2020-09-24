d5be40523bd97c1f8a71d12aace1d6cf77c53062
Arlecuno <palchinskij19@gmail.com>
2020-09-24 18:37:40 +0300

    start homework 4;

---
+++ JS/homework/homework_4/script.js
@@ -0,0 +1,54 @@
+//1
+
+// function isLink(link) {
+//    if (link !== ' ' && link.length >= 10) {
+//       return link;
+//    } else {
+//       return null;
+//    }
+// }
+
+// function isNumber(number) {
+//    if (!isNaN(number)) {
+//       return number;
+//    } else {
+//       return null;
+//    }
+// }
+
+// function createImage(link, angle) {
+
+//    if (isLink(link) && isNumber(angle)) {
+//       let image = document.createElement('img');
+//       image.classList.add('myimg');
+//       image.setAttribute('src', link);
+//       image.setAttribute('alt', 'random image');
+
+//       image.style.transform = `rotate(${angle}deg)`;
+
+//       document.body.appendChild(image);
+//    }
+// }
+
+// function start(numberOfImages) {
+//    for (let i = 0; i < numberOfImages; i++) {
+//       const questionImage = prompt('Дай ссылку!');
+//       const questionAngle = +prompt('На сколько градусов повернуть?');
+
+//       createImage(questionImage, questionAngle);
+//    }
+// }
+// const numberOfImg = +prompt('Сколько картинок создать?');
+
+// start(numberOfImg);
+
+//--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+//2 
+
+function hasTag(tag) {
+   const tagId = document.querySelector(tag);
+   return document.body.contains(tagId) ? tagId.remove() : alert('Тега нет на странице!');
+}
+
+hasTag('div');
