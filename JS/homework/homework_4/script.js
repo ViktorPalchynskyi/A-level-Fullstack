0118dac015ffcaaaabbc5c2b6e86da3625004e06
Arlecuno <palchinskij19@gmail.com>
2020-09-28 17:41:07 +0300

    homework 3

--- JS/homework/homework_4/script.js
+++ JS/homework/homework_4/script.js
@@ -1,51 +1,51 @@
 //1
 
-function isLink(link) {
-   if (link !== ' ' && link.length >= 10) {
-      return link;
-   } else {
-      return null;
-   }
-}
+// function isLink(link) {
+//    if (link !== ' ' && link.length >= 10) {
+//       return link;
+//    } else {
+//       return null;
+//    }
+// }
 
-function isNumber(number) {
-   if (!isNaN(number)) {
-      return number;
-   } else {
-      return null;
-   }
-}
+// function isNumber(number) {
+//    if (!isNaN(number)) {
+//       return number;
+//    } else {
+//       return null;
+//    }
+// }
 
-function createImage(link, angle) {
+// function createImage(link, angle) {
 
-   if (isLink(link) && isNumber(angle)) {
-      let image = document.createElement('img');
-      image.classList.add('myimg');
-      image.setAttribute('src', link);
-      image.setAttribute('alt', 'random image');
+//    if (isLink(link) && isNumber(angle)) {
+//       let image = document.createElement('img');
+//       image.classList.add('myimg');
+//       image.setAttribute('src', link);
+//       image.setAttribute('alt', 'random image');
 
-      image.style.transform = `rotate(${angle}deg)`;
+//       image.style.transform = `rotate(${angle}deg)`;
 
-      document.body.appendChild(image);
-   }
-}
+//       document.body.appendChild(image);
+//    }
+// }
 
-function start(numberOfImages) {
-   const questionImage = prompt('Дай ссылку!');
-   const questionAngle = +prompt('На сколько градусов повернуть?');
+// function start(numberOfImages) {
+//    const questionImage = prompt('Дай ссылку!');
+//    const questionAngle = +prompt('На сколько градусов повернуть?');
 
-   let angleSum = 0;
+//    let angleSum = 0;
 
-   for (let i = 0; i < numberOfImages; i++) {
+//    for (let i = 0; i < numberOfImages; i++) {
 
-      angleSum += questionAngle;
+//       angleSum += questionAngle;
 
-      createImage(questionImage, angleSum);
-   }
-}
-const numberOfImg = +prompt('Сколько картинок создать?');
+//       createImage(questionImage, angleSum);
+//    }
+// }
+// const numberOfImg = +prompt('Сколько картинок создать?');
 
-start(numberOfImg);
+// start(numberOfImg);
 
 //--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
@@ -57,5 +57,55 @@ function hasTag(tag) {
 }
 
 hasTag('div');
+
+//--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+//4
+
+
+
+//--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+//6
+
+var persons = [{
+   name: 'Vasyan',
+   age: 21
+}, {
+   name: 'Kolyan',
+   age: 22
+}, {
+   name: 'Mashka',
+   age: 17
+}];
+
+function sort(obj, val) {
+   let sortedArr = [];
+
+   for (let item of obj) {
+      for (let key in item) {
+         if (key === val) {
+            sortedArr[sortedArr.length] = item[key];
+         }
+      }
+   }
+
+   return sortedArr;
+}
+
+console.log(sort(persons, 'name'));
+
+//--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+//7
+
+const arr = [' hello darkness   my old   friend ', ' i\'ve come to   talk  with   you again    '];
+
+const newArr = arr.map((item) => {
+   console.log(item.trim());
+});
+
+
+
 
 //--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
