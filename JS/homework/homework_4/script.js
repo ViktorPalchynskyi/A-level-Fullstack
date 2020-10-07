1c461b27defc7ccfcf8125dddbb7bfdd25f993a4
Arlecuno <palchinskij19@gmail.com>
2020-10-07 22:26:31 +0300

    finished task 7

--- JS/homework/homework_4/script.js
+++ JS/homework/homework_4/script.js
@@ -1,55 +1,55 @@
-//1
+// //1
 
-function isLink(link) {
-   if (link !== ' ' && link.length >= 10) {
-      return link;
-   } else {
-      return null;
-   }
-}
+// function isLink(link) {
+//    if (link !== ' ' && link.length >= 10) {
+//       return link;
+//    } else {
+//       return null;
+//    }
+// }
 
-function isNumber(number) {
-   if (!isNaN(number)) {
-      return number;
-   } else {
-      return null;
-   }
-}
+// function isNumber(number) {
+//    if (!isNaN(number)) {
+//       return number;
+//    } else {
+//       return null;
+//    }
+// }
 
-function createImage(link, angle) {
+// function createImage(link, angle) {
 
-   if (isLink(link) && isNumber(angle)) {
-      let image = document.createElement('img');
-      image.classList.add('myimg');
-      image.setAttribute('src', link);
-      image.setAttribute('alt', 'random image');
+//    if (isLink(link) && isNumber(angle)) {
+//       let image = document.createElement('img');
+//       image.classList.add('myimg');
+//       image.setAttribute('src', link);
+//       image.setAttribute('alt', 'random image');
 
-      image.style.transform = `rotate(${angle}deg)`;
+//       image.style.transform = `rotate(${angle}deg)`;
 
-      document.body.appendChild(image);
-   }
-}
+//       document.body.appendChild(image);
+//    }
+// }
 
-function start(numberOfImages) {
-   const questionImage = prompt('Дай ссылку!');
-   const questionAngle = +prompt('На сколько градусов повернуть?');
+// function start(numberOfImages) {
+//    const questionImage = prompt('Дай ссылку!');
+//    const questionAngle = +prompt('На сколько градусов повернуть?');
 
-   let angleSum = 0;
+//    let angleSum = 0;
 
-   for (let i = 0; i < numberOfImages; i++) {
+//    for (let i = 0; i < numberOfImages; i++) {
 
-      angleSum += questionAngle;
+//       angleSum += questionAngle;
 
-      createImage(questionImage, angleSum);
-   }
-}
-const numberOfImg = +prompt('Сколько картинок создать?');
+//       createImage(questionImage, angleSum);
+//    }
+// }
+// const numberOfImg = +prompt('Сколько картинок создать?');
 
-start(numberOfImg);
+// start(numberOfImg);
 
-//--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+// //--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-//2 
+// //2 
 
 // function hasTag(tag) {
 //    const tagId = document.querySelector(tag);
@@ -58,45 +58,44 @@ start(numberOfImg);
 
 // hasTag('div');
 
-//--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+// //--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-//3/4
+// //3/4
 
-let table = document.createElement('table');
-let tr;
-let td;
+// let table = document.createElement('table');
+// let tr;
+// let td;
 
-for (let i = 1; i < 11; i++) {
-   tr = document.createElement('tr');
-   for (let j = 1; j < 11; j++) {
-      td = document.createElement('td');
-      td.style.border = "1px solid black";
-      td.innerText = `${i} * ${j} = ${i * j}`;
-      tr.appendChild(td);
-      console.log(td);
-   }
-   table.appendChild(tr);
-}
-document.body.appendChild(table);
+// for (let i = 1; i < 11; i++) {
+//    tr = document.createElement('tr');
+//    for (let j = 1; j < 11; j++) {
+//       td = document.createElement('td');
+//       td.style.border = "1px solid black";
+//       td.innerText = `${i} * ${j} = ${i * j}`;
+//       tr.appendChild(td);
+//    }
+//    table.appendChild(tr);
+// }
+// document.body.appendChild(table);
 
-const tds = document.querySelectorAll('td');
+// const tds = document.querySelectorAll('td');
 
-function addColor(elem, event, color) {
-   elem.forEach(item => {
-      item.addEventListener(event, () => {
-         item.style.backgroundColor = color;
-      });
-   });
-}
+// function addColor(elem, event, color) {
+//    elem.forEach(item => {
+//       item.addEventListener(event, () => {
+//          item.style.backgroundColor = color;
+//       });
+//    });
+// }
 
-addColor(tds, 'mouseover', 'tomato');
-addColor(tds, 'mouseout', 'white');
-
+// addColor(tds, 'mouseover', 'tomato');
+// addColor(tds, 'mouseout', 'white');
 
 
-//--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-//5
+// //--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+// //5
 
 // const number1 = document.querySelector('[name = "number1"]');
 // const number2 = document.querySelector('[name = "number2"]');
@@ -121,9 +120,9 @@ addColor(tds, 'mouseout', 'white');
 //    res.innerText = `result = ${sum}`;
 // });
 
-//--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+// //--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-//6
+// //6
 
 // var persons = [{
 //    name: 'Vasyan',
@@ -152,9 +151,9 @@ addColor(tds, 'mouseout', 'white');
 
 // console.log(sort(persons, 'name'));
 
-//--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+// //--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-//7
+// //7
 
 // const regExp1 = (/\s\s\s/g);
 // const regExp2 = (/\s\s/g);
@@ -167,4 +166,28 @@ addColor(tds, 'mouseout', 'white');
 // }
 
 
+// //--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+// //7
+
+function filter(array, cb) { // should return new filtered array
+   let filtredArray = [];
+
+   for (let i = 0; array.length > i; i++) {
+      if (cb(array[i])) {
+         filtredArray.push(array[i]);
+      }
+   }
+   return filtredArray;
+}
+let arr1 = [10, -21, 0, -2, 31, 41, 41];
+
+const test = (n) => {
+   if (n > 0) {
+      return n;
+   }
+};
+
+console.log(filter(arr1, test));
+console.log(arr1.filter(item => item > 0));
 //--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
